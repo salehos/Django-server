7289a4cd51c5ba4584b9d2947e710972c9979a18
mohammad saleh <salehos763@gmail.com>
2018-11-21 15:13:37 +0330

    DOROST ADD KARDANE HTML O CSS

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def index(request):
+    pass
--- templates/README.md
+++
@@ -1,4 +0,0 @@
-# mahfood
-HTML Templates for webelopers django course 2018
-
-Use these templates to create custome html pages for django backend project.
